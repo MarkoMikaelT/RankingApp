95d6add0680b2c76730c121e072a12128123d9d7
Marko Tiitto <marko.tiitto@hotmail.fi>
2023-01-11 15:15:18 +0200

    Added image to RankItems page
    RankItems now has example movie images put on the page

---
+++ ClientApp/src/components/MovieImages.js
@@ -0,0 +1,16 @@
+﻿import Matti from '../Images/movieImages/Mattiiii.jpg';
+import NewMario from '../Images/movieImages/NewMario.jpg';
+import OldMario from '../Images/movieImages/OldMario.jpg';
+import Sharknado2 from '../Images/movieImages/Sharknado2.jpg';
+import Sharknado5 from '../Images/movieImages/Sharknado5.jpg';
+import TheRealsharknado from '../Images/movieImages/TheRealsharknado.jpg';
+
+const MovieImgArr = [
+    { id: 0, image: Matti },
+    { id: 1, image: NewMario },
+    { id: 2, image: OldMario },
+    { id: 3, image: Sharknado2 },
+    { id: 4, image: Sharknado5 },
+    { id: 5, image: TheRealsharknado }
+];
+export default MovieImgArr;
--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,4 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react'
+import MovieImgArr from './MovieImages';
+
 
 const RankItems = () => {
 
@@ -18,10 +20,16 @@ const RankItems = () => {
 
     return (
         <main>
+            <div className="items-not-ranked">
             {
-                (items.length > 0) ? items.map((item) => <h3>{item.title}</h3>) : <div>Loading....</div>
+                (items.length > 0) ? items.map((item) =>
+                    /*<h3>{item.title}</h3>*/
+                    <div className="unranked-cell">
+                        <img id={`item-${item.id}`} src={MovieImgArr.find(i => i.id === item.id)?.image } />
+                    </div>
+                    ) : <div>Loading....</div>
             }
-            
+            </div>
         </main>    
     )
 }
