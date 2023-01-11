50752b950e55b6c2177de0331b313ac4f63fa531
Marko Tiitto <marko.tiitto@hotmail.fi>
2023-01-11 18:17:11 +0200

    added rankingGrid
    rankingGrid done started implementing item dragging. Dragging not working currently

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -1,11 +1,34 @@
 ﻿import React, { useState, useEffect } from 'react'
-import MovieImgArr from './MovieImages';
+import MovieImgArr from './MovieImages'
+import RankingGrid from './RankingGrid'
 
 
 const RankItems = () => {
 
     const [items, setItems] = useState([]);
     const dataType = 0;
+
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id)
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault()
+    }
+
+    function drop(ev) {
+        ev.preventDefault();
+        const targetElm = ev.target
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5))
+            const transformedRow = items.map((item) => (item.id === parseInt(data)) ?
+                { ...item, rank: parseInt(targetElm.id.substring(5)) } : {...item, rank: item-rank})
+            setItems(transformedRow)
+        }
+    }
 
     useEffect(() => {
         fetch(`item/${dataType}`)
@@ -20,12 +43,15 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImgArr} drag={drag} allowDrop={allowDrop} drop={drop} />
             <div className="items-not-ranked">
             {
                 (items.length > 0) ? items.map((item) =>
                     /*<h3>{item.title}</h3>*/
                     <div className="unranked-cell">
-                        <img id={`item-${item.id}`} src={MovieImgArr.find(i => i.id === item.id)?.image } />
+                        <img id={`item-${item.id}`} src={MovieImgArr.find(i => i.id === item.id)?.image}
+                            style={{ cursor: "pointer" }} draggable="true" onDragStart={drag}
+                        />
                     </div>
                     ) : <div>Loading....</div>
             }
---
+++ ClientApp/src/components/RankingGrid.js
@@ -0,0 +1,93 @@
+﻿
+const RankingGrid = ({ items, imgArr, drag, allowDrop, drop }) => {
+
+    const rankingGrid = []
+
+    const topTier = []
+    const neartopTier = []
+    const middleTier = []
+    const bottomTier = []
+    const worstTier = []
+
+
+    function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) {
+        if (rankNum > 0) {
+            var item = items.find(k => k.ranking === rankNum)
+            cellCollection.push(<div id={`rank-${rankNum}`} className="rank-cell" onDrop={drop} onDragOver={allowDrop} >
+                {(item != null) ? <img id={`item-${item.id}`} src={imgArr.find(k => k.id === item.id)?.img} draggable="true" onDragStart={drag} />
+                                : null}
+            </div>)
+        }
+        else {
+            cellCollection.push(<div className="row-label">
+                <h4>{rowLabel}</h4>
+            </div>)
+        }
+    }
+
+    function createCellsForRow(rowNum) {
+        var rankNum = 0
+        var currTier = []
+        var label = ""
+        const numCells = 5
+
+        for (var i = 1; i <= numCells; i++) {
+            rankNum = (i === 1) ? 0 : (numCells * (rowNum - 1)) + i - rowNum
+
+            switch (rowNum) {
+                case 1:
+                    currTier = topTier
+                    label = "Top Tier"
+                    break;
+                case 2:
+                    currTier = neartopTier
+                    label = "Almost Top Tier"
+                    break;
+                case 3:
+                    currTier = middleTier
+                    label = "OK Tier"
+                    break;
+                case 4:
+                    currTier = bottomTier
+                    label = "OK. Tier"
+                    break;
+                case 5:
+                    currTier = worstTier
+                    label = "Never again Tier"
+                    break;
+            }
+            pushCellMarkupToArr(currTier, rankNum, label)
+            
+        }
+    }
+
+    function createRowCells() {
+        const maxRows = 5;
+        for (var i = 1; i <= maxRows; i++) {
+            createCellsForRow(i)
+        }
+    }
+
+    function createGridRows() {
+        rankingGrid.push(<div className="rank-row top-tier">{topTier}</div>)
+        rankingGrid.push(<div className="rank-row neartop-tier">{neartopTier}</div>)
+        rankingGrid.push(<div className="rank-row middle-tier">{middleTier}</div>)
+        rankingGrid.push(<div className="rank-row bottom-tier">{bottomTier}</div>)
+        rankingGrid.push(<div className="rank-row worst-tier">{worstTier}</div>)
+
+        return rankingGrid
+    }
+
+    function createRankingGrid() {
+        createRowCells()
+        return createGridRows()
+    }
+
+    return (
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>    
+    )
+
+}
+export default RankingGrid;
